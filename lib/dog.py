08aee9996f655fdc877b87c983599da7d7b8e856
Efjeniah <esaru008@gmail.com>
2024-03-16 09:45:49 +0300

    Completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name=name
+        self.breed=breed
+        # print(f"{name} is born!")
+
+#     # def showing_self(self):
+#     #     return self
+
+    # def bark(self):
+    #     print("Woof!")
+
+    # def get_adopted(self, owner_name):
+    #     self.owner=owner_name
+        
+#     def adopt(dog, owner_name):
+#         dog.owner = owner_name
+
+# adopt(fido, "Sophie")
+
+fido=Dog("Fido")
+fido.breed
+# fido.name
+# # fido.showing_self()
+# # fido.bark("fido")
+snoopy=Dog("Snoopy", "Terrier")
+snoopy.breed
+
+# fido.owner="Sophie"
+# fido.get_adopted("Sophie")
+# fido.owner
